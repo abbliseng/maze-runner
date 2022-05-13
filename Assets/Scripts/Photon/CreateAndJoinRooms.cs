86b7be190ab6367b756661740afb06f44c7d4f41
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-05-14 00:07:02 +0200

    broke a bunch of stuff with the new camera

--- Assets/Scripts/Photon/CreateAndJoinRooms.cs
+++ Assets/Scripts/Photon/CreateAndJoinRooms.cs
@@ -21,6 +21,6 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("Mazeboi");
+        PhotonNetwork.LoadLevel("Main");
     }
 }
